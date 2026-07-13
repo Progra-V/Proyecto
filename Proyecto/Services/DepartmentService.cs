3c706a113f54d00f07de782976897534baeb7c25
Barrex29 <kevinbc29@hotmail.com>
2026-07-12 20:44:43 -0600

    Modulo Department creado

---
+++ Proyecto/Services/DepartmentService.cs
@@ -0,0 +1,91 @@
+﻿using Supabase;
+using Proyecto.Models;
+using Proyecto.SupabaseClient;
+
+namespace Proyecto.Services
+{
+    public static class DepartmentService
+    {
+        public static async Task<List<Department>> GetAll()
+        {
+            Client client = SupabClient.getSupabaseClient();
+
+            await client.InitializeAsync();
+
+            var result = await client
+                .From<Department>()
+                .Get();
+
+            return result.Models;
+        }
+
+
+        public static async Task<Department?> GetById(int id)
+        {
+            Client client = SupabClient.getSupabaseClient();
+
+            await client.InitializeAsync();
+
+            var result = await client
+                .From<Department>()
+                .Where(x => x.Id == id)
+                .Get();
+
+            return result.Model;
+        }
+
+
+        public static async Task<Department?> GetByName(string nombre)
+        {
+            try
+            {
+                Client client = SupabClient.getSupabaseClient();
+
+                await client.InitializeAsync();
+
+                var result = await client
+                    .From<Department>()
+                    .Where(x => x.Nombre == nombre)
+                    .Single();
+
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public static async Task Create(Department department)
+        {
+            Client client = SupabClient.getSupabaseClient();
+
+            await client.InitializeAsync();
+
+            await client
+                .From<Department>()
+                .Insert(department);
+        }
+
+
+        public static async Task Edit(Department department)
+        {
+            Client client = SupabClient.getSupabaseClient();
+
+            await client.InitializeAsync();
+
+            await client
+                .From<Department>()
+                .Update(department);
+        }
+
+
+        public static async Task ChangeStatus(Department department)
+        {
+            department.Activo = !department.Activo;
+
+            await Edit(department);
+        }
+    }
+}
