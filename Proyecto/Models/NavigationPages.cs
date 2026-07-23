ec2ea0c544f1f1236e79e2a3335c7ae82bda51c0
Steven <155604212+StevenX506@users.noreply.github.com>
2026-07-23 01:09:18 -0600

    Multiples correciones

--- Proyecto/Models/NavigationPages.cs
+++ Proyecto/Models/NavigationPages.cs
@@ -11,5 +11,7 @@
         public string Section { get; set; } = string.Empty;
 
         public string Icon { get; set; } = string.Empty;
+
+        public bool RequiresConfirmation { get; set; }
     }
 }
